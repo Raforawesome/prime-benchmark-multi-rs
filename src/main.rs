8bdb0d85625b81d5b80c16da6dd819f537a14305
Raforawesome <ubuntuhasnoroblox@gmail.com>
2022-03-29 18:28:28 -0400

    autobuild + initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
 use std::time::{ Instant };
 use std::io;
 use std::env;
+use std::thread;
+use std::thread::JoinHandle;
 
 fn main() {
 	let start = Instant::now();
-	for i in 1..=5000000 {
-		if is_prime(i as f64) {
-			println!("{} is a prime number", i);
-		}
+	let max: i32 = 40000000;
+	let mut thread_handles: Vec<JoinHandle<i32>> = Vec::new();
+	let mut prime_count: i32 = 0;
+	let threads = 16;
+
+	for i in 1..=threads {
+		let sub_sect: i32 = max / threads * i;
+		let pre_sect: i32 = max / threads * (i - 1);
+		thread_handles.push(thread::spawn(move || {
+			let mut c: i32 = 0;
+			for i in pre_sect..=sub_sect {
+				if is_prime(i as f64) {
+					c += 1;
+				}
+			}
+			c
+		}));
 	}
+	for handle in thread_handles {
+		let res: i32 = handle.join().unwrap();
+		prime_count += res;
+	}
+
+	println!("Found {} primes between {} and {}", prime_count, 1, max);
 	let delta_time = start.elapsed();
 	println!("Time elapsed: {:?}", delta_time);
-
-
-	if env::consts::OS == "windows" {
-		let mut tmp: String = String::from("");
-		println!("{}", "Press any key to exit the program...");
-		let tmp2 = io::stdin().read_line(&mut tmp);
-
-		std::mem::drop(tmp);
-		std::mem::drop(tmp2);
-	}
 }
 
 fn is_prime(num: f64) -> bool {
